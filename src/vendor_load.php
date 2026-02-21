23767dc0c489284afb66838291a7d6b5d973b6ba
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-21 06:55:03 +0300

    Update vendor_load.php

--- src/vendor_load.php
+++ src/vendor_load.php
@@ -1,7 +1,7 @@
 <?PHP
 
 if (substr(__DIR__, 0, 2) == 'I:') {
-    include_once 'I:/mdwiki/auth_repo/vendor_load.php';
+    include_once 'I:/mdwiki/auth_repo/src/vendor_load.php';
 } else {
     include_once __DIR__ . '/../auth/vendor_load.php';
 }
