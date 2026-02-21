1438a903c8659dc5aa419ac2b236cedfa22abf92
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-21 06:53:27 +0300

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+// Set test environment
+putenv('APP_ENV=testing');
+putenv('DB_HOST=localhost:3306');
+putenv('DB_NAME=s54732__mdwiki');
+putenv('DB_NAME_NEW=s54732__mdwiki_new');
+putenv('TOOL_TOOLSDB_USER=root');
+putenv('TOOL_TOOLSDB_PASSWORD=root11');
+$_SERVER['SERVER_NAME'] = 'localhost';
 $vendorAutoload = __DIR__ . '/../vendor/autoload.php';
 
 if (file_exists($vendorAutoload)) {
@@ -11,6 +19,3 @@ if (file_exists($vendorAutoload)) {
 // Don't load include.php in tests as it tries to connect to the database
 // Instead, we'll load only the specific files we need for testing
 
-// Set test environment
-putenv('APP_ENV=testing');
-$_SERVER['SERVER_NAME'] = 'localhost';
