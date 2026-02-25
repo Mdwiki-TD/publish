3b5bb784440a60e8a64de4db83e06c4097721faf
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-25 05:42:35 +0300

    Update vendor_load.php

--- src/vendor_load.php
+++ src/vendor_load.php
@@ -2,9 +2,9 @@
 
 $vendor_path = __DIR__ . '/vendor/autoload.php';
 if (!file_exists($vendor_path)) {
-    $vendor_path = __DIR__ . '/../vendor/autoload.php';
+    $vendor_path = dirname(__DIR__) . '/vendor/autoload.php';
 }
 if (!file_exists($vendor_path)) {
-    $vendor_path = __DIR__ . '/../auth/vendor_load.php';
+    $vendor_path = dirname(__DIR__) . '/auth/vendor_load.php';
 }
 require $vendor_path;
