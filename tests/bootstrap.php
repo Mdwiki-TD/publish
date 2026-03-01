b9fbb9fb88d150de2dba7f08ce46f50fd873fc22
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-01 07:47:46 +0300

    Use DB_HOST_TOOLS and remove test debug code
    Read the DB host from DB_HOST_TOOLS instead of DB_HOST in src/bots/mdwiki_sql.php. Remove ad-hoc test/debug ini blocks from mdwiki_sql.php and include.php to avoid uncontrolled debug output. Update tests/bootstrap.php to set DB_HOST_TOOLS=localhost:3306 so tests continue to run with the new env var naming. Minor cleanup of commented usage lines in mdwiki_sql.php.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 // Set test environment
 putenv('APP_ENV=testing');
-putenv('DB_HOST=localhost:3306');
+putenv('DB_HOST_TOOLS=localhost:3306');
 putenv('DB_NAME=s54732__mdwiki');
 putenv('DB_NAME_NEW=s54732__mdwiki_new');
 putenv('TOOL_TOOLSDB_USER=root');
 putenv('TOOL_TOOLSDB_PASSWORD=root11');
 $_SERVER['SERVER_NAME'] = 'localhost';
+
 $vendorAutoload = __DIR__ . '/../vendor/autoload.php';
 
 if (file_exists($vendorAutoload)) {
