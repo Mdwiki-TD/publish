e4e4d9cf3518067b14ecc58d28ec0f8afb61b8f3
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-10 07:22:13 +0300

    .

--- tests/integration/process/ProcessEditTest.php
+++ tests/integration/process/ProcessEditTest.php
@@ -8,5 +8,30 @@ use PHPUnit\Framework\TestCase;
 
 class ProcessEditTest extends TestCase
 {
+    public function testNamespaceConstants(): void
+    {
+        $userNamespace = 2;
+        $mainNamespace = 0;
+        $categoryNamespace = 14;
+
+        $this->assertSame(2, $userNamespace);
+        $this->assertSame(0, $mainNamespace);
+        $this->assertSame(14, $categoryNamespace);
+    }
+
+    public function testNamespaceComparison(): void
+    {
+        $ns = 2;
+        $isUserPage = ($ns == 2);
+        $this->assertTrue($isUserPage);
+    }
+
+    public function testNonUserNamespaceIsNotUserPage(): void
+    {
+        $ns = 0;
+        $isUserPage = ($ns == 2);
+        $this->assertFalse($isUserPage);
+    }
+
     public static function setUpBeforeClass(): void {}
 }
--- tests/integration/process/StartTest.php
+++ tests/integration/process/StartTest.php
@@ -4,10 +4,47 @@ namespace Tests\Bots;
 
 use PHPUnit\Framework\TestCase;
 
-/**
- * Tests for start.php
- */
 class StartTest extends TestCase
 {
+    public function testFormatUserNormalisesUnderscores(): void
+    {
+        $user = str_replace("_", " ", "John_Doe");
+        $this->assertSame('John Doe', $user);
+    }
+
+    public function testFormatTitleNormalisesUnderscores(): void
+    {
+        $title = str_replace("_", " ", "My_Article");
+        $this->assertSame('My Article', $title);
+    }
+
+    public function testMakeSummaryContainsRevid(): void
+    {
+        $summary = "Created by translating the page [[:mdwiki:Special:Redirect/revision/123|Test]] to:en #mdwikicx";
+        $this->assertStringContainsString('123', $summary);
+    }
+
+    public function testDetermineHashtagEmptyForMrIbrahem(): void
+    {
+        $title = "Mr. Ibrahem/SomePage";
+        $user = "Mr. Ibrahem";
+        $hashtag = "";
+        if (strpos($title, "Mr. Ibrahem") !== false && $user == "Mr. Ibrahem") {
+            $hashtag = "";
+        }
+        $this->assertSame('', $hashtag);
+    }
+
+    public function testDetermineHashtagDefaultForOtherUser(): void
+    {
+        $title = "SomeArticle";
+        $user = "OtherUser";
+        $hashtag = "#mdwikicx";
+        if (strpos($title, "Mr. Ibrahem") !== false && $user == "Mr. Ibrahem") {
+            $hashtag = "";
+        }
+        $this->assertSame('#mdwikicx', $hashtag);
+    }
+
     public static function setUpBeforeClass(): void {}
 }
