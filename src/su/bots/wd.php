bdf3af3b76babaa1d331913c57dc217ee9e10f71
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-10 06:21:07 +0300

    Update wd.php

--- src/su/bots/wd.php
+++ src/su/bots/wd.php
@@ -11,34 +11,6 @@ use function Publish\AccessHelps\get_user_access;
 use function Publish\Helps\pub_test_print;
 
 
-function LinkIt($qid, $lang, $sourcetitle, $targettitle, $access_key, $access_secret)
-{
-    $wikidata_domain = getenv('WIKIDATA_DOMAIN') ?: ($_ENV['WIKIDATA_DOMAIN'] ?? 'www.wikidata.org');
-    $https_domain = "https://$wikidata_domain";
-    $apiParams = [
-        "action" => "wbsetsitelink",
-        "linktitle" => $targettitle,
-        "linksite" => "{$lang}wiki",
-    ];
-    if (!empty($qid)) {
-        $apiParams["id"] = $qid;
-    } else {
-        $apiParams["title"] = $sourcetitle;
-        $apiParams["site"] = "enwiki";
-    }
-    $response = post_params($apiParams, $https_domain, $access_key, $access_secret);
-    $Result = json_decode($response, true) ?? [];
-    // if (isset($Result->error)) {
-    if (isset($Result['error'])) {
-        pub_test_print("post_params: Result->error: " . json_encode($Result['error']));
-    }
-    if ($Result == null) {
-        pub_test_print("post_params: Error: " . json_last_error() . " " . json_last_error_msg());
-        pub_test_print("response:");
-        pub_test_print($response);
-    }
-    return $Result;
-}
 function getAccessCredentials($user, $access_key, $access_secret)
 {
     if (!$access_key || !$access_secret) {
@@ -54,6 +26,25 @@ function getAccessCredentials($user, $access_key, $access_secret)
     return [$access_key, $access_secret];
 }
 
+function LinkIt($apiParams, $access_key, $access_secret)
+{
+    $wikidata_domain = getenv('WIKIDATA_DOMAIN') ?: ($_ENV['WIKIDATA_DOMAIN'] ?? 'www.wikidata.org');
+    $https_domain = "https://$wikidata_domain";
+
+    $response = post_params($apiParams, $https_domain, $access_key, $access_secret);
+
+    $Result = json_decode($response, true) ?? [];
+    if (isset($Result['error'])) {
+        pub_test_print("post_params: Result->error: " . json_encode($Result['error']));
+    }
+
+    if (empty($Result)) {
+        pub_test_print("post_params: Error: " . json_last_error() . " " . json_last_error_msg());
+        pub_test_print("response:");
+        pub_test_print($response);
+    }
+    return $Result;
+}
 function LinkToWikidata($sourcetitle, $lang, $user, $targettitle, $access_key, $access_secret)
 {
     $qids = GetQidForMdtitle($sourcetitle);
@@ -65,7 +56,19 @@ function LinkToWikidata($sourcetitle, $lang, $user, $targettitle, $access_key, $
     }
     list($access_key, $access_secret) = $credentials;
 
-    $link_result = LinkIt($qid, $lang, $sourcetitle, $targettitle, $access_key, $access_secret) ?? [];
+    $apiParams = [
+        "action" => "wbsetsitelink",
+        "linktitle" => $targettitle,
+        "linksite" => "{$lang}wiki",
+    ];
+    if (!empty($qid)) {
+        $apiParams["id"] = $qid;
+    } else {
+        $apiParams["title"] = $sourcetitle;
+        $apiParams["site"] = "enwiki";
+    }
+
+    $link_result = LinkIt($apiParams, $access_key, $access_secret) ?? [];
 
     $link_result["qid"] = $qid;
 
