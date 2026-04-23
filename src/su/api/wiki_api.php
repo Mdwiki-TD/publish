4592b2c84565fd269e761c7ace4b6994350da387
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-23 04:19:49 +0300

    Update wiki_api.php

--- src/su/api/wiki_api.php
+++ src/su/api/wiki_api.php
@@ -21,11 +21,14 @@ function GetTitleInfo($targettitle, $lang)
     $url = "https://$lang.wikipedia.org/w/api.php" . "?" . http_build_query($params, '', '&', PHP_QUERY_RFC3986);
     pub_test_print("GetTitleInfo url: $url");
     try {
-        $result = get_url_curl($url);
-        pub_test_print("GetTitleInfo result: $result");
-        $result = json_decode($result, true);
+        $output = get_url_curl($url);
+        pub_test_print("GetTitleInfo result: $output");
+        $result = json_decode($output, true);
+
+        if (!is_array($result)) return null;
+
         // { "query": { "pages": [ { "pageid": 5049507, "ns": 2, "title": "利用者:Mr. Ibrahem/オランザピン/サミドルファン" } ] } }
-        $result = $result['query']['pages'][0];
+        $result = $result['query']['pages'][0] ?: null;
     } catch (\Exception $e) {
         pub_test_print("GetTitleInfo: $e");
         $result = null;
