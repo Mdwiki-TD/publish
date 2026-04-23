b39e221f095aeb873dc5efcbdcc7d7245180be00
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-23 05:28:43 +0300

    Update text_edit.php

--- src/su/text_edit.php
+++ src/su/text_edit.php
@@ -19,11 +19,11 @@ function get_lang_settings($lang_code)
 function text_changes($sourcetitle, $title, $text, $lang, $mdwiki_revid)
 {
     if (function_exists('\WpRefs\FixPage\fix_page_with_setting')) {
-        $settings = get_lang_settings($lang);
+        $settings = get_lang_settings($lang) ?: [];
 
-        $move_dots = $settings['move_dots'] ?: null;
-        $expand = $settings['expend'] ?: null;
-        $add_en_lang = $settings['add_en_lang'] ?: null;
+        $move_dots = $settings['move_dots'] ?? null;
+        $expand = $settings['expend'] ?? null;
+        $add_en_lang = $settings['add_en_lang'] ?? null;
 
         $newtext = \WpRefs\FixPage\fix_page_with_setting(
             $sourcetitle,
